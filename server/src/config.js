f14ae24c0fd69f779669cc799beac5e0b1ae1bed
makebacappiello <makebacappiello@gmail.com>
2025-08-04 17:43:25 -0400

    created endpointys for addone and getall suggestions

--- server/src/config.js
+++ server/src/config.js
@@ -1,5 +1,6 @@
 const config = {
-  databaseUrl: "YOUR DATABASE CONNECTION STRING GOES HERE",
+  databaseUrl:
+    "postgresql://neondb_owner:npg_mSLdWglOw57N@ep-wandering-dust-aetoj8c2-pooler.c-2.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
 };
 
 export default config;
---
+++ server/src/index.js
@@ -0,0 +1,70 @@
+// ---------------------------------
+// Boilerplate Code to Set Up Server
+// ---------------------------------
+
+// Importing our Node modules
+import express from "express";
+
+// The framework that lets us easily build a web server
+import pg from "pg"; // pg stands for PostgreSQL, for talking to the database
+import config from "./config.js"; // we need access to our database connection credentials
+
+// connect to our PostgreSQL database, or db for short
+const db = new pg.Pool({
+  connectionString: config.databaseUrl, // credentials to access the database — keep this private!
+  ssl: true, // we will use SSL encryption when connecting to the database
+});
+
+const app = express(); // Creating an instance of the express module
+
+app.use(express.json()); // This server will receive and respond in JSON format
+
+const port = 3000; // Declaring which port to listen to to receive requests
+
+// Turning on our server to listen for requests
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+// ---------------------------------
+// Helper Functions
+// ---------------------------------
+// Helper function for /get-all-suggestions
+
+async function getAllSuggestions() {
+  const result = await db.query("SELECT * FROM suggestions");
+  console.log(result);
+  return result.rows;
+}
+// Helper function for /add-one-suggestion
+
+async function addOneSuggestion(suggestions) {
+  await db.query(
+    "INSERT INTO suggestions (title, description, category, upvotes, status) VALUES ($1, $2, $3, $4, $5 )",
+    [
+      suggestions.title,
+      suggestions.description,
+      suggestions.category,
+      suggestions.upvotes,
+      suggestions.status,
+    ]
+  );
+}
+
+// ---------------------------------
+// API Endpoints
+// ---------------------------------
+
+// GET /get-all-suggestions
+app.get("/get-all-suggestions", async (req, res) => {
+  const allSuggestions = await getAllSuggestions();
+  // res.send(JSON.stringify(allSuggestions));
+  res.json(allSuggestions);
+});
+
+// POST /add-one-suggestion
+app.post("/add-one-suggestion", async (req, res) => {
+  const newSuggestion = req.body;
+  await addOneSuggestion(newSuggestion);
+  res.send("The new suggestion was successfully added!");
+});
